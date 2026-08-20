b0890128a89b120b2796f00c9629e1144b736638
Tsavoritel <tsavoritel@disroot.org>
2026-08-20 12:25:22 +0300

    added combinations.py and added warm hint to RNGame

---
+++ ExcplicitlyOma/Mod03/Combinations.py
@@ -0,0 +1,4 @@
+import random
+code3 = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+code4 = str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6))
+print(f"3-digit code: {code3} \n4-digit code: {code4}")
--- Extra/RNGame.py
+++ Extra/RNGame.py
@@ -1,16 +1,18 @@
 import random
 randmin = 1
-randmax = 20
-rand = int(random.randint(1, 20))
+randmax = 50
+rand = int(random.randint(randmin, randmax))
 
-cd = 3 #"close delta" or how far away should the program consider a number to be close
-answerrange = range(rand-cd, rand+cd)
+hd = 3 #"hot delta" or how far away should the program consider a number to be close
+wd = 12 #warm delta
+answerhotrange = range(rand-hd, rand+hd)
+answerwarmrange = range(rand-wd, rand+wd)
 hint = ""
 
 guesscnt = 0
 usrguess = 0
 
-print(f"A random number was generated (1-{randmax})")
+print(f"A random number was generated ({randmin}-{randmax})")
 
 def guess():
     try:
@@ -27,9 +29,11 @@ def processguess(usrg):
         print(f"Got it !! You guessed {guesscnt} time(s) !!")
         exit
     else:
-        yourrange = range(usrg-cd, usrg+cd)
-        if range(max(yourrange[0], answerrange[0]), min(yourrange[-1], answerrange[-1])+1):
-            hint = "you are close !!"
+        yourrange = range(usrg-hd, usrg+hd)
+        if usrg in answerhotrange:
+            hint = "you are hot !!"
+        elif usrg in answerwarmrange:
+            hint = "you are warm."
         else:
             hint = "you are far away..."
         print(f"You guessed wrongly {guesscnt} time(s) !!")
