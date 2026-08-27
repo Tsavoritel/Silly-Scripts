f71c5e571ba4c6b291e4e69c5545b36d1cda6751
Tsavoritel <tsavoritel@disroot.org>
2026-08-27 10:55:28 +0300

    removed useless code on approxpi

--- ExcplicitlyOma/Mod05/ApproxPi.py
+++ ExcplicitlyOma/Mod05/ApproxPi.py
@@ -1,11 +1,7 @@
 import math
 import random
 
-x = []
-y = []
-
 maxn = int(input("How many points to generate? ")) # N
-# maxh = int(input("Max random number to generate? "))
 
 n = 0 # n thats in circle A
 c = 0 # counter
@@ -13,11 +9,9 @@ c = 0 # counter
 while (c < maxn):
     px = random.random() #points in unknown square
     py = random.random()#1, maxh)
-    if math.pow(px, 2)+math.pow(py, 2) < 1: #is it in the circle also?
-        x.append(px)
-        y.append(py)
-        n += 1
     c += 1
+    if math.pow(px, 2)+math.pow(py, 2) < 1: #is it in the circle also?
+        n += 1
     if c % (maxn / 100) == 0:
         percent = (c / maxn) * 100
         print(f"Loading: {round(percent)}%")
