dca069368315ef47760ffea9899c0501eeaf93ed
Tsavoritel <tsavoritel@disroot.org>
2026-09-16 12:17:43 +0300

    Updated project to Project 3

--- ExcplicitlyOma/Project.py
+++ ExcplicitlyOma/Project.py
@@ -1,25 +1,41 @@
-#mod01 excercise1, mod02 excercise2, and project 1-2
+#mod01 excercise1, mod02 excercise2, and project 1-3
+
 name = input("Enter your name: ")
+uin = ""
+items = []
+
+#age check
+
 try:
     age = int(input("...and your age? "))
 except:
     print("Please input a whole number.")
-uin = ""
 if age < 13:
     print("You are a minor, and are not yet old enough to use this product")
     print("Shutting down...")
     exit
-else:
-    print(f"Hello {name}, age {age} !") # space is intentional
-    while uin != "lopeta":
-        print("Options: yay, whoami, lopeta")
-        uin = input("Input: ")
-        match uin:
-            case "yay":
-                print("Yaayyyyyy!!!!!!!")
-            case "whoami":
-                print(f"You are {name} and are {age} years old.")
-            case "lopeta":
-                print("Exiting...")
-            case _:
-                print("Invalid option, please choose from the following list")
+
+#main program
+
+def ItemAdder(item):
+    items.append(item)
+def CheckInventory():
+    print(items)
+def PlayerInfo():
+    print(f"You are {name} and are {age} years old.")
+
+print(f"Hello {name}, age {age} !")
+while uin != "lopeta":
+    print("Options: gotitem, inventory, whoami, lopeta")
+    uin = input("Input: ")
+    match uin:
+        case "gotitem":
+            ItemAdder(input("What item did you pickup? "))
+        case "inventory":
+            CheckInventory()
+        case "whoami":
+            PlayerInfo()
+        case "lopeta":
+            print("Exiting...")
+        case _:
+            print("Invalid option, please choose from the following list")
